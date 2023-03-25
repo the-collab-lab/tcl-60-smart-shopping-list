3ca271e995dff870e3772a2cc161779cddcc62b4
Stacie Taylor <stacietaylorcima@gmail.com>
2023-03-25 13:11:16 -0700

    Update config.js

--- src/api/config.js
+++ src/api/config.js
@@ -3,13 +3,12 @@ import { getFirestore } from 'firebase/firestore';
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-	apiKey: 'FILL_ME_IN',
-	authDomain: 'FILL_ME_IN',
-	databaseURL: 'FILL_ME_IN',
-	projectId: 'FILL_ME_IN',
-	storageBucket: 'FILL_ME_IN',
-	messagingSenderId: 'FILL_ME_IN',
-	appId: 'FILL_ME_IN',
+  apiKey: "AIzaSyDr7J31UUmaFBRlYlZk1HNE5uHUC6-Vjt0",
+  authDomain: "tcl-60-smart-shopping-list.firebaseapp.com",
+  projectId: "tcl-60-smart-shopping-list",
+  storageBucket: "tcl-60-smart-shopping-list.appspot.com",
+  messagingSenderId: "84161723862",
+  appId: "1:84161723862:web:75f457a43cc66bb8bbb877"
 };
 
 // Initialize Firebase
