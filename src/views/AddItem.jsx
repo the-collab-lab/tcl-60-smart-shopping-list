a1fd562c8f2929b77a9a0afbace2e0ce01f70251
shahx95 <24467345+shahx95@users.noreply.github.com>
2023-04-10 20:47:52 +0500

    Add form and form handle functions

--- src/views/AddItem.jsx
+++ src/views/AddItem.jsx
@@ -1,10 +1,31 @@
+import React from 'react';
+
 export function AddItem() {
+	const [formData, setFormData] = React.useState({
+		itemName: '',
+		daysUntilNextPurchase: '',
+	});
+
+	function handleChange(event) {
+		event.preventDefault();
+
+		console.log('Something changed');
+	}
 	return (
 		<>
 			<p>
 				Hello from the <code>/add-item</code> page!
 			</p>
-			{/* Add form here */}
+			<form>
+				<input
+					type="text"
+					onChange={handleChange}
+					name="itemName"
+					value={formData.itemName}
+				/>
+
+				<button>Submit</button>
+			</form>
 		</>
 	);
 }
