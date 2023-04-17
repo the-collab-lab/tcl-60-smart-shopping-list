f106f212b614d462ba685b29b0d7afe8cdcce4cd
Tooba <jtooba70@gmail.com>
2023-04-17 12:35:42 +0500

    Add form

--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -1,8 +1,10 @@
 import './Home.css';
 import { useNavigate } from 'react-router-dom';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
+import { checkItem } from '../api/firebase';
 
 export function Home({ setNewToken, token }) {
+	const [existingToken, setExistingToken] = useState('');
 	const navigate = useNavigate();
 	// useEffect will watch for changes to the token and will redirect whenever it exists
 	useEffect(() => {
@@ -10,7 +12,16 @@ export function Home({ setNewToken, token }) {
 		// disable warning that navigate should be a dependency
 		// eslint-disable-next-line
 	}, [token]);
+	function handleTokenChange(e) {
+		setExistingToken(e.target.value);
+	}
 
+	function handleSubmit(e) {
+		e.preventDefault();
+		checkItem(existingToken);
+		console.log('SUBMITTED');
+	}
+	console.log(existingToken);
 	return (
 		<div className="Home">
 			<p>
@@ -18,6 +29,16 @@ export function Home({ setNewToken, token }) {
 			</p>
 			<button onClick={setNewToken}>New List</button>
 			{/* Search list form */}
+			<form onSubmit={handleSubmit}>
+				<label htmlFor="existingToken">Existing token</label>
+				<input
+					type="text"
+					id="existingToken"
+					value={existingToken}
+					onChange={handleTokenChange}
+				/>
+				<button>Join existing list</button>
+			</form>
 		</div>
 	);
 }
