5a5b645dccef35e20fad45f877c22aa56feffed2
Tooba <jtooba70@gmail.com>
2023-04-24 11:58:06 +0500

    Prompt user to add a new item if list is empty
    Co-authored-by: Laura Abro <l.abrocadabro@gmail.com>

--- src/views/List.jsx
+++ src/views/List.jsx
@@ -1,7 +1,9 @@
 import { ListItem } from '../components';
 import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 
 export function List({ data, token }) {
+	const navigate = useNavigate();
 	const [query, setQuery] = useState('');
 	const handleChange = (event) => {
 		setQuery(event.target.value);
@@ -11,27 +13,41 @@ export function List({ data, token }) {
 			<p>
 				Hello from the <code>/list</code> page!
 			</p>
-			{/* welcome message */}
-			<div>
-				<label htmlFor="query">Filter your list</label>
-				<input id="query" value={query} onChange={handleChange} name="query" />
-				{query ? <button onClick={() => setQuery('')}>Clear</button> : null}
-			</div>
-			<ul>
-				{data
-					.filter((item) =>
-						item.name.toLowerCase().includes(query.toLowerCase()),
-					)
-					.map((item) => (
-						<ListItem key={item.id} name={item.name} />
-					))}
-			</ul>
-			<div>
-				<p>
-					Use the token <em style={{ textDecoration: 'underline' }}>{token}</em>{' '}
-					to share your shopping list
-				</p>
-			</div>
+			{!data.length ? (
+				<div>
+					<p>Your list is empty click the add item button to start your list</p>
+					<button onClick={() => navigate('/add-item')}>Add Item</button>
+				</div>
+			) : (
+				<>
+					<div>
+						<label htmlFor="query">Filter your list</label>
+						<input
+							id="query"
+							value={query}
+							onChange={handleChange}
+							name="query"
+						/>
+						{query ? <button onClick={() => setQuery('')}>Clear</button> : null}
+					</div>
+					<ul>
+						{data
+							.filter((item) =>
+								item.name.toLowerCase().includes(query.toLowerCase()),
+							)
+							.map((item) => (
+								<ListItem key={item.id} name={item.name} />
+							))}
+					</ul>
+					<div>
+						<p>
+							Use the token{' '}
+							<em style={{ textDecoration: 'underline' }}>{token}</em> to share
+							your shopping list
+						</p>
+					</div>
+				</>
+			)}
 		</>
 	);
 }
